87d4616289806e6e1687dd4a31d70917215d82c9
namrap17 <namrap17@gmail.com>
2021-03-08 23:39:37 +0530

    new learning of res and req

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    const url = req.url;
+    if(url === '/') {
+        res.write("HELLO");
+     
+    } else if (url === '/hey') {
+        res.write('Hey');
+    }
+   return  res.end();
+
+});
+
+server.listen(3000);
